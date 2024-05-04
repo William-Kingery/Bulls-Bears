3a94c81a92fbcaf2cf8ab8d2f676720d411c2f4a
William-Kingery <william.kingery@gmail.com>
2024-05-04 08:46:25 -0400

    routes established

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import user from "./routes/user.js";
 import home from "./routes/home.js";
-import profile from "./routes/profile.js"
+import profile from "./routes/profile.js";
+import news from "./routes/news.js"
+import trending from "./routes/trending.js"
 
 
 const app = express();
@@ -19,6 +21,8 @@ app.route("/")
 app.use("/user", user)
 app.use('/home', home)
 app.use('/profile', profile)
+app.use('/news', news)
+app.use('/trending', trending)
 
 
 app.listen(PORT || 5050, () => {
---
+++ server/routes/news.js
@@ -0,0 +1,12 @@
+import express from "express";
+import knex from 'knex';
+import knexfile from '../knexfile.js';
+
+const router = express.Router();
+const myknex = knex(knexfile);
+
+router.get("/", (req, res) => {
+    res.status(200).json("Here's the news");
+  })
+
+export default router;
