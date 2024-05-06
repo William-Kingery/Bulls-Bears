b8f7daec76398ee365474540116a12bbc3601698
William-Kingery <william.kingery@gmail.com>
2024-05-06 17:08:50 -0400

    Ticker component setup and NPM react-ticker installed

---
+++ client/src/components/Ticker/Ticker.js
@@ -0,0 +1,13 @@
+
+import "./Ticker.scss"
+
+
+const Ticker = () => {
+    return (
+        <div>
+            ticker
+        </div>
+    )
+}
+
+export default Ticker 
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,13 +2,15 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Calendar from "../../components/Calendar/Calendar";
+import Ticker from "../../components/Ticker/Ticker";
 
 const Home = () => {
-    return (
-        <div>
-            this is home
-             <Calendar/>
-        </div>
+   return (
+      <div>
+         this is /home
+         <Ticker />
+         <Calendar/>
+      </div>
     );
 }
 
