6cc27e5ef397b14a1367e41452fafca84984104d
William-Kingery <william.kingery@gmail.com>
2024-05-08 20:32:20 -0400

    Data rendered in calendar cells, but having problem with API

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Calendar.scss"
+
+const URL = 'http://localhost:8080/'
 
 const Calendar = () => {
    const currentDate = new Date();
    const [selectedDate, setSelectedDate] = useState(currentDate);
+   const [earnings, setEarnings] = useState([]);
+
+   useEffect(() => {
+      const earningsData = async (date) => {
+         try {
+            const response = await axios.get(`${URL}home/earnings`, {
+               params: {
+                  month: date.getMonth() + 1,
+                  year: date.getFullYear(),
+               },
+            });
+            const parsedData = response.data.split('\n').slice(1).map(line => {
+               const [symbol, name, reportDate, fiscalDateEnding, estimate, currency] = line.split(',');
+               return { symbol, name, reportDate, fiscalDateEnding, estimate, currency };
+             });
+            setEarnings(parsedData);
+            console.log(parsedData)
+          } catch (error) {
+            console.error("Error fetching events:", error);
+         }
+        };
+        earningsData(selectedDate);
+   }, [selectedDate]);
 
    const daysInMonth = (date) => {
       return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
@@ -29,21 +55,36 @@ const Calendar = () => {
       const numDays = daysInMonth(selectedDate);
       const firstDay = firstDayOfMonth(selectedDate);
       const calendar = [];
-
+  
       for (let i = 0; i < firstDay; i++) {
          calendar.push(<div key={`empty-${i}`} className="calendar__cell calendar__empty"></div>);
       }
       for (let day = 1; day <= numDays; day++) {
-         calendar.push(
+         const earningsForDay = earnings.filter((earning) => {
+            const earningDate = new Date(earning.reportDate);
+            return (earningDate.getFullYear() === selectedDate.getFullYear() &&
+            earningDate.getMonth() === selectedDate.getMonth() &&
+            earningDate.getDate() === day);
+         });
+        calendar.push(
          <div
             key={day}
-            className={`calendar__cell ${selectedDate.getDate() === day ? "calendar__selected" : ""}`}
+            className={`calendar__cell ${
+               selectedDate.getDate() === day ? "calendar__selected" : ""
+            }`}
             onClick={() => handleDateClick(day)}
-            >
-            {day}
+         >
+         {day}
+         <div className="earnings">
+            {earningsForDay.map((earning, index) => (
+               <div key={index} className="earning">
+                  {earning.symbol}
+               </div>
+            ))}
+            </div>
          </div>
       );
-      }
+   }
       return calendar;
    };
 
@@ -60,3 +101,34 @@ const Calendar = () => {
 };
 
 export default Calendar;
+
+
+
+
+
+
+
+
+
+
+// const generateCalendar = () => {
+//    const numDays = daysInMonth(selectedDate);
+//    const firstDay = firstDayOfMonth(selectedDate);
+//    const calendar = [];
+
+//    for (let i = 0; i < firstDay; i++) {
+//       calendar.push(<div key={`empty-${i}`} className="calendar__cell calendar__empty"></div>);
+//    }
+//    for (let day = 1; day <= numDays; day++) {
+//       calendar.push(
+//       <div
+//          key={day}
+//          className={`calendar__cell ${selectedDate.getDate() === day ? "calendar__selected" : ""}`}
+//          onClick={() => handleDateClick(day)}
+//          >
+//          {day}
+//       </div>
+//    );
+//    }
+//    return calendar;
+// };
